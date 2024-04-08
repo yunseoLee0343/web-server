65364f89c12b2523eeb2689fab911ff7a866b382
Yunseo Lee <lys139011@gmail.com>
2024-04-09 04:56:12 +0900

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        HTTPServer server = HTTPServer(80);
+        server.serve("sample-docroot", Map.ofEntries(
+            Map.entry("index", "index.html"),
+            Map.entry("gallery", "gallery.html"),
+            Map.entry("400", "400.html"),
+            Map.entry("404", "404.html"))
+        );
+        // Handle server close with ctrl+c in terminal
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            server.close();
+        }));
+        // Start the server
+        server.serve_forever();
+    }
+}
